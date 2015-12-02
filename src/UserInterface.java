64008426fc49aead3c1c6e1d0c2ab2e1e1a12bee
Rabea Gleissner <rabeagleissner@gmail.com>
2015-12-02 15:41:55 +0000

    adds messages for user interface

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,5 +1,15 @@
+import java.util.Objects;
+
 public class UserInterface {
     public String askForHumanChoice() {
-        return "Please enter your choice: R = rock, S = scissors, P = paper";
+        return "Please enter your choice: (R = rock, S = scissors, P = paper) or type E for exit";
+    }
+
+    public String announceGameEnd(String winner) {
+        if (Objects.equals(winner, "draw")) {
+            return "It's a draw!";
+        } else {
+            return "Winner: " + winner;
+        }
     }
 }
--- test/UserInterfaceTest.java
+++ test/UserInterfaceTest.java
@@ -1,3 +1,4 @@
+import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,8 +14,22 @@ public class UserInterfaceTest {
 
     @Test
     public void askForHumanChoice() {
-        assertEquals("Please enter your choice: R = rock, S = scissors, P = paper",
+        assertEquals("Please enter your choice: (R = rock, S = scissors, P = paper) or type E for exit",
                 userInterface.askForHumanChoice());
+    }
 
+    @Test
+    public void announceGameDrawn() {
+        assertEquals("It's a draw!", userInterface.announceGameEnd("draw"));
+    }
+
+    @Test
+    public void announceHumanAsWinner() {
+        assertEquals("Winner: human", userInterface.announceGameEnd("human"));
+    }
+
+    @Test
+    public void announceComputerAsWinner() {
+        assertEquals("Winner: computer", userInterface.announceGameEnd("computer"));
     }
 }
