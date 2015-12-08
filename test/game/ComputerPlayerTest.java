edca00fe610ed26eb86ea70cee932a17cc49820f
Rabea Gleissner <rabeagleissner@gmail.com>
2015-12-08 18:07:04 +0000

    deleted unnecessary test

--- test/game/ComputerPlayerTest.java
+++ test/game/ComputerPlayerTest.java
@@ -4,7 +4,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class ComputerPlayerTest {
     private ComputerPlayer computerPlayer;
@@ -14,16 +13,6 @@ public class ComputerPlayerTest {
     public void setup() {
        fakeRandomizer = new FakeRandomizer();
        computerPlayer = new ComputerPlayer(fakeRandomizer);
-    }
-
-    @Test
-    public void generatesMove() {
-        fakeRandomizer.setFakeRandomMove(Move.ROCK);
-        assertTrue(
-                (Move.ROCK).equals(computerPlayer.generateMove()) ||
-                (Move.SCISSORS).equals(computerPlayer.generateMove()) ||
-                (Move.PAPER).equals(computerPlayer.generateMove())
-        );
     }
 
     @Test
